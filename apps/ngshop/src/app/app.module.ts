71cc2c71932dd921ade6fede8692aa942f0ca23d
Markus Machel <machelmarkus@gmail.com>
2022-02-18 22:01:13 -0300

    fCom

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { UiModule } from '@bluebits/ui';
+import { AccordionModule } from 'primeng/accordion';
 
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+{path: 'products', component: ProductsListComponent}
+]
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HomePageComponent, ProductsListComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(routes), UiModule, AccordionModule],
   providers: [],
   bootstrap: [AppComponent],
 })
